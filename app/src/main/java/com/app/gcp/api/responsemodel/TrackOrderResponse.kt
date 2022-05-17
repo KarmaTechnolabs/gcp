e9f780c4ddbc2d27e5cbec00849e3569f9fea840
KarmaTechnolabs <karmatechnolabs@gmail.com>
2022-05-17 20:41:31 +0530

    change track order api response

--- app/src/main/java/com/app/gcp/api/responsemodel/TrackOrderResponse.kt
+++ app/src/main/java/com/app/gcp/api/responsemodel/TrackOrderResponse.kt
@@ -4,11 +4,11 @@ import com.google.gson.annotations.SerializedName
 
 data class TrackOrderResponse(
     @SerializedName("history")
-    val history: History?,
+    val history: List<History>?,
     @SerializedName("order_details")
     val orderDetails: OrderDetails?,
     @SerializedName("order_stages")
-    val orderStages: OrderStages?,
+    val orderStages: List<OrderStage>?,
     @SerializedName("order_stages_history")
     val orderStagesHistory: List<OrderStagesHistory>?,
     @SerializedName("order_tracking_number")
@@ -16,8 +16,28 @@ data class TrackOrderResponse(
 )
 
 data class History(
-    @SerializedName("7")
-    val x7: List<X7>?
+    @SerializedName("color")
+    val color: String?,
+    @SerializedName("created_at")
+    val createdAt: String?,
+    @SerializedName("deleted")
+    val deleted: String?,
+    @SerializedName("id")
+    val id: String?,
+    @SerializedName("is_current")
+    val isCurrent: String?,
+    @SerializedName("notes")
+    val notes: String?,
+    @SerializedName("order_id")
+    val orderId: String?,
+    @SerializedName("sort")
+    val sort: String?,
+    @SerializedName("stage_id")
+    val stageId: String?,
+    @SerializedName("title")
+    val title: String?,
+    @SerializedName("updated_at")
+    val updatedAt: String?
 )
 
 data class OrderDetails(
@@ -65,77 +85,20 @@ data class OrderDetails(
     val trackingNumber: String?
 )
 
-data class OrderStages(
-    @SerializedName("client_id")
-    val clientId: String?,
-    @SerializedName("created_at")
-    val createdAt: String?,
-    @SerializedName("created_by")
-    val createdBy: String?,
-    @SerializedName("deleted")
-    val deleted: String?,
-    @SerializedName("discount_amount")
-    val discountAmount: String?,
-    @SerializedName("discount_amount_type")
-    val discountAmountType: String?,
-    @SerializedName("discount_type")
-    val discountType: String?,
-    @SerializedName("excess_quantity")
-    val excessQuantity: String?,
-    @SerializedName("files")
-    val files: String?,
-    @SerializedName("gst_amount")
-    val gstAmount: String?,
-    @SerializedName("gst_percentage")
-    val gstPercentage: String?,
+data class OrderStage(
     @SerializedName("id")
     val id: String?,
-    @SerializedName("note")
-    val note: String?,
-    @SerializedName("order_date")
-    val orderDate: String?,
-    @SerializedName("project_id")
-    val projectId: String?,
-    @SerializedName("shipping_address")
-    val shippingAddress: String?,
-    @SerializedName("stage_id")
-    val stageId: String?,
-    @SerializedName("status_id")
-    val statusId: String?,
-    @SerializedName("tax_id")
-    val taxId: String?,
-    @SerializedName("tax_id2")
-    val taxId2: String?,
-    @SerializedName("tracking_number")
-    val trackingNumber: String?
-)
-
-data class OrderStagesHistory(
-    @SerializedName("color")
-    val color: String?,
-    @SerializedName("created_at")
-    val createdAt: String?,
-    @SerializedName("deleted")
-    val deleted: String?,
-    @SerializedName("id")
-    val id: String?,
-    @SerializedName("is_current")
-    val isCurrent: String?,
-    @SerializedName("notes")
-    val notes: String?,
     @SerializedName("order_id")
     val orderId: String?,
-    @SerializedName("sort")
-    val sort: String?,
+    @SerializedName("order_main_id")
+    val orderMainId: String?,
     @SerializedName("stage_id")
     val stageId: String?,
     @SerializedName("title")
-    val title: String?,
-    @SerializedName("updated_at")
-    val updatedAt: String?
+    val title: String?
 )
 
-data class X7(
+data class OrderStagesHistory(
     @SerializedName("color")
     val color: String?,
     @SerializedName("created_at")
